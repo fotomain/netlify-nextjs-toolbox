9e43f80526fef32bffbf82d0fe8e6bbb5f631c78
fotomain <foto888999@gmail.com>
2023-11-05 21:46:03 +0200

    111

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -1,42 +1,42 @@
 // Jokes provided from the lovely folks at https://icanhazdadjoke.com
 import jokes from './jokes.json';
 
-// export default async (req, ctx) => {
-//     if (req.method === "POST")
-//         { const res = ctx.json({ message: 'you posted!' });
-//             res.headers.set("Access-Control-Allow-Origin", "*");
-//             res.headers.append("Access-Control-Allow-Headers", "*");
-//             res.headers.append("Access-Control-Allow-Methods", "*");
-//             return res;
-//         } else if (req.method === "OPTIONS")
-//         {
-//             const res = new Response();
-//             const randomIndex = Math.floor(Math.random() * jokes.length)
-//             const randomJoke = 'jokes[randomIndex]'
-//             res.body = JSON.stringify(randomJoke)
-//             res.headers.set("Access-Control-Allow-Origin", "*");
-//             res.headers.append("Access-Control-Allow-Headers", "*");
-//             res.headers.append("Access-Control-Allow-Methods", "*");
-//             res.headers.append("Content-Type","application/json");
-//             return res;
-//         }
-// };
-
-export const handler = async (event) => {
-    // Generates a random index based on the length of the jokes array
-    const randomIndex = Math.floor(Math.random() * jokes.length)
-    const randomJoke = jokes[randomIndex]
-
-    // Netlify Functions need to return an object with a statusCode
-    // Other properties such as headers or body can also be included.
-    return {
-        statusCode: 200,
-        body: JSON.stringify(randomJoke),
-        headers:{
-            "Access-Control-Allow-Headers": "*",
-            // "Access-Control-Allow-Methods": "*",
-            // "Content-Type": "application/json"
+export default async (req, ctx) => {
+    if (req.method === "POST")
+        { const res = ctx.json({ message: 'you posted!' });
+            res.headers.set("Access-Control-Allow-Origin", "*");
+            res.headers.append("Access-Control-Allow-Headers", "*");
+            res.headers.append("Access-Control-Allow-Methods", "*");
+            return res;
+        } else if (req.method === "OPTIONS")
+        {
+            const res = new Response();
+            const randomIndex = Math.floor(Math.random() * jokes.length)
+            const randomJoke = 'jokes[randomIndex]'
+            res.body = JSON.stringify(randomJoke)
+            res.headers.set("Access-Control-Allow-Origin", "*");
+            res.headers.append("Access-Control-Allow-Headers", "*");
+            res.headers.append("Access-Control-Allow-Methods", "*");
+            res.headers.append("Content-Type","application/json");
+            return res;
         }
-    }
-}
+};
+
+// export const handler = async (event) => {
+//     // Generates a random index based on the length of the jokes array
+//     const randomIndex = Math.floor(Math.random() * jokes.length)
+//     const randomJoke = jokes[randomIndex]
+//
+//     // Netlify Functions need to return an object with a statusCode
+//     // Other properties such as headers or body can also be included.
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify(randomJoke),
+//         headers:{
+//             "Access-Control-Allow-Headers": "*",
+//             // "Access-Control-Allow-Methods": "*",
+//             // "Content-Type": "application/json"
+//         }
+//     }
+// }
 
