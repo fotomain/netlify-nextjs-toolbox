68ec606cbadec4641b420e6ce77d47e19e097b17
fotomain <foto888999@gmail.com>
2023-11-06 18:44:45 +0200

    ff

--- netlify/functions/joke_bearer.js
+++ netlify/functions/joke_bearer.js
@@ -10,45 +10,54 @@ export default async (req, ctx) => {
     if (req.method === "POST")
     {
 
+        var oo_='---'
+        const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
+        if (allowedOrigins.includes(origin)) {
+            // res.setHeader('Access-Control-Allow-Origin', origin);
+            oo_=origin
+        }
+
         const req_data = await req.json();
         const res = ctx.json({
             statusCode: 200,
+            origin: oo_,
             message: '!!!!!!!!!!!!!!! you posted!',
             req_data: req_data,
-    });
-        // res.headers.set("Access-Control-Allow-Origin", "*");
-        // res.headers.append("Access-Control-Allow-Headers", "*");
-        // res.headers.append("Access-Control-Allow-Methods", "*");
+        });
+
+        res.headers.set("Access-Control-Allow-Origin", "*");
+        res.headers.append("Access-Control-Allow-Headers", "*");
+        res.headers.append("Access-Control-Allow-Methods", "*");
 
 
         //ccccccccccccccc
-        const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
-        if (allowedOrigins.includes(origin)) {
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        }
-        // res.headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
-
-        res.headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.headers.append("Access-Control-Allow-Methods", "*");
-        res.headers.append("Access-Control-Allow-Credentials", true);
+        // const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
+        // if (allowedOrigins.includes(origin)) {
+        //     res.setHeader('Access-Control-Allow-Origin', origin);
+        // }
+        // // res.headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+        //
+        // res.headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        // res.headers.append("Access-Control-Allow-Methods", "*");
+        // res.headers.append("Access-Control-Allow-Credentials", true);
 
         return res;
     } else if (req.method === "OPTIONS")
     {
         const res = new Response();
-        // res.headers.set("Access-Control-Allow-Origin", "*");
-        // res.headers.append("Access-Control-Allow-Headers", "*");
-        // res.headers.append("Access-Control-Allow-Methods", "*");
+        res.headers.set("Access-Control-Allow-Origin", "*");
+        res.headers.append("Access-Control-Allow-Headers", "*");
+        res.headers.append("Access-Control-Allow-Methods", "*");
 
         //ccccccccccccc
-        const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
-        if (allowedOrigins.includes(origin)) {
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        }
-        // res.headers.set("Access-Control-Allow-Origin", "");
-        res.headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.headers.append("Access-Control-Allow-Methods", "*");
-        res.headers.append("Access-Control-Allow-Credentials", true);
+        // const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
+        // if (allowedOrigins.includes(origin)) {
+        //     res.setHeader('Access-Control-Allow-Origin', origin);
+        // }
+        // // res.headers.set("Access-Control-Allow-Origin", "");
+        // res.headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        // res.headers.append("Access-Control-Allow-Methods", "*");
+        // res.headers.append("Access-Control-Allow-Credentials", true);
 
         return res;
 
