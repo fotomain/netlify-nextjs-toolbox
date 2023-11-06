73983e5db106fda08ad727067859bd6baa9cae40
fotomain <foto888999@gmail.com>
2023-11-06 19:10:32 +0200

    ff

--- netlify/functions/joke_bearer1_ok.js
+++ netlify/functions/joke_bearer1_ok.js
@@ -4,19 +4,21 @@ export default async (req, ctx) => {
     if (req.method === "POST")
     {
 
+        var oo_='---'
+        const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
+        if (allowedOrigins.includes(req.origin)) {
+            // res.setHeader('Access-Control-Allow-Origin', origin);
+            oo_=req.origin
+        }
         const req_data = await req.json();
         const res = ctx.json({
             statusCode: 200,
             message: '!!!!!!!!!!!!!!! you posted!',
             req_data: req_data,
+            reqorigin: oo_,
         });
 
-        // var oo_='---'
-        // const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
-        // if (allowedOrigins.includes(origin)) {
-        //     // res.setHeader('Access-Control-Allow-Origin', origin);
-        //     oo_=origin
-        // }
+
         //
         // const req_data = await req.json();
         // const res = ctx.json({
