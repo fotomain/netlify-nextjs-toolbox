e6e1658cb6fa119923a89191e250a237c96dfc0f
fotomain <foto888999@gmail.com>
2023-11-06 12:52:15 +0200

    ff

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -3,11 +3,13 @@ import jokes from './jokes.json';
 
 export default async (req, ctx) => {
     if (req.method === "POST")
-        { const res = ctx.json({ message: '!!!!!!!!!!!!!!! you posted!' });
-            res.headers.set("Access-Control-Allow-Origin", "*");
-            res.headers.append("Access-Control-Allow-Headers", "*");
-            res.headers.append("Access-Control-Allow-Methods", "*");
-            return res;
+        {
+            return jokes[0]
+            // const res = ctx.json({ message: '!!!!!!!!!!!!!!! you posted!' });
+            // res.headers.set("Access-Control-Allow-Origin", "*");
+            // res.headers.append("Access-Control-Allow-Headers", "*");
+            // res.headers.append("Access-Control-Allow-Methods", "*");
+            // return res;
         } else if (req.method === "OPTIONS")
         {
             const res = new Response();
