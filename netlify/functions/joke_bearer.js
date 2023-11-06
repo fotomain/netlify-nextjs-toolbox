f69265236b63b91e56a507173024c69eb63f2d93
fotomain <foto888999@gmail.com>
2023-11-06 12:53:43 +0200

    ff

--- netlify/functions/joke_bearer.js
+++ netlify/functions/joke_bearer.js
@@ -2,9 +2,11 @@
 
 export default async (req, ctx) => {
     if (req.method === "POST")
-    { const res = ctx.json({
+    {
+        const req_data = await req.json();
+        const res = ctx.json({
         message: '!!!!!!!!!!!!!!! you posted!',
-        req_data: await req.json(),
+        // req_data: req_data,
     });
         res.headers.set("Access-Control-Allow-Origin", "*");
         res.headers.append("Access-Control-Allow-Headers", "*");
