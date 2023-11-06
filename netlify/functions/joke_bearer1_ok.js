9b21e3072365acd494ee74e965a654347369a8d7
fotomain <foto888999@gmail.com>
2023-11-06 18:58:30 +0200

    ff

--- netlify/functions/joke_bearer1_ok.js
+++ netlify/functions/joke_bearer1_ok.js
@@ -4,12 +4,30 @@ export default async (req, ctx) => {
     if (req.method === "POST")
     {
 
+        // const req_data = await req.json();
+        // const res = ctx.json({
+        //     statusCode: 200,
+        //     message: '!!!!!!!!!!!!!!! you posted!'
+        //     // req_data: req_data,
+        // });
+
+        var oo_='---'
+        const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
+        if (allowedOrigins.includes(origin)) {
+            // res.setHeader('Access-Control-Allow-Origin', origin);
+            oo_=origin
+        }
+
         const req_data = await req.json();
         const res = ctx.json({
             statusCode: 200,
-            message: '!!!!!!!!!!!!!!! you posted!'
-            // req_data: req_data,
+            origin: oo_,
+            message: '!!!!!!!!!!!!!!! you posted!',
+            req_data: req_data,
         });
+
+
+
         res.headers.set("Access-Control-Allow-Origin", "*");
         res.headers.append("Access-Control-Allow-Headers", "*");
         res.headers.append("Access-Control-Allow-Methods", "*");
