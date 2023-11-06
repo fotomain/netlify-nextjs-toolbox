17bdd864741c660fefe886c813eb275f4700a9aa
fotomain <foto888999@gmail.com>
2023-11-06 17:37:30 +0200

    ff

--- netlify/functions/joke_bearer.js
+++ netlify/functions/joke_bearer.js
@@ -11,17 +11,33 @@ export default async (req, ctx) => {
             message: '!!!!!!!!!!!!!!! you posted!',
             req_data: req_data,
     });
+        // res.headers.set("Access-Control-Allow-Origin", "*");
+        // res.headers.append("Access-Control-Allow-Headers", "*");
+        // res.headers.append("Access-Control-Allow-Methods", "*");
+
+
+        //ccccccccccccccc
         res.headers.set("Access-Control-Allow-Origin", "*");
-        res.headers.append("Access-Control-Allow-Headers", "*");
+        res.headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         res.headers.append("Access-Control-Allow-Methods", "*");
+        res.headers.append("Access-Control-Allow-Credentials", "true");
+
         return res;
     } else if (req.method === "OPTIONS")
     {
         const res = new Response();
+        // res.headers.set("Access-Control-Allow-Origin", "*");
+        // res.headers.append("Access-Control-Allow-Headers", "*");
+        // res.headers.append("Access-Control-Allow-Methods", "*");
+
+        //ccccccccccccc
         res.headers.set("Access-Control-Allow-Origin", "*");
-        res.headers.append("Access-Control-Allow-Headers", "*");
+        res.headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         res.headers.append("Access-Control-Allow-Methods", "*");
+        res.headers.append("Access-Control-Allow-Credentials", "true");
+
         return res;
+
     }
 
 
