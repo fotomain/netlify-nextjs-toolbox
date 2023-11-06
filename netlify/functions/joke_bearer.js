b44376e0a008be823ce3fef69171533663fd8047
fotomain <foto888999@gmail.com>
2023-11-06 10:42:22 +0200

    ff

--- netlify/functions/joke_bearer.js
+++ netlify/functions/joke_bearer.js
@@ -1,22 +1,37 @@
+
 // Jokes provided from the lovely folks at https://icanhazdadjoke.com
 import jokes from './jokes.json';
 
+import {NextResponse} from 'next/server'
+
 export default async (req, ctx) => {
     if (req.method === "POST")
-    { const res = ctx.json({ message: '!!!!!!!!!!!!!!! you posted!' });
+    {
+        const res = ctx.json({ message: '!!!!!!!!!!!!!!! you posted!' });
         res.headers.set("Access-Control-Allow-Origin", "*");
         res.headers.append("Access-Control-Allow-Headers", "*");
         res.headers.append("Access-Control-Allow-Methods", "*");
         return res;
+
     } else if (req.method === "OPTIONS")
+        // https://community.auth0.com/t/how-to-validate-a-token-on-next-js-backend-from-a-separate-frontend/103328/2
     {
+        const token = req.headers.get("Authorization");
         const res = new Response();
         res.headers.set("Access-Control-Allow-Origin", "*");
         //222
-        res.headers.append("Access-Control-Allow-Headers", "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept");
-        // res.headers.append("Access-Control-Allow-Headers", "*");
+        // res.headers.append("Access-Control-Allow-Headers", "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept");
+        res.headers.append("Access-Control-Allow-Headers", "*");
         res.headers.append("Access-Control-Allow-Methods", "*");
-        return res;
+
+        if(token==="!!!!!!!bearercode777999!!!!!!")
+            return res;
+        else{
+            return new NextResponse(
+                JSON.stringify({success: false, message: '!!!!! No bearer Authentication failed: No token given'}),
+                {status: 401, headers: {'content-type': 'application/json'}}
+            );
+        }
     }
 
 
