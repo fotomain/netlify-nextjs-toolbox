ba2c1c9f08b2ef1ba9d0efbfad2f1082d991e93d
fotomain <foto888999@gmail.com>
2023-11-07 11:39:53 +0200

    ff

--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,14 @@ import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
-    <Head>
-        <title>Next.js api</title>
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-}
-
-function Home1() {
-  return (
     <div className="container">
-      <Head>
-        <title>Next.js Toolbox</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <Header title="Next.js Toolbox" />
-        <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
-        <FeedbackForm />
-        <JokeBlock />
-      </main>
-      <Footer />
+        <Head>
+            <title>Next.js api</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
     </div>
-  );
+
 }
+
