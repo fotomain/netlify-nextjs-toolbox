3cc762e841dd5ccc893d8c6becebf323b8b17109
fotomain <foto888999@gmail.com>
2023-11-07 13:39:18 +0200

    ff

--- netlify/functions/sendemail.js
+++ netlify/functions/sendemail.js
@@ -33,15 +33,27 @@ export default async (req, ctx) => {
         };
         res.port_emails_sended_model = port_emails_sended_model;
 
-        await collection_emails_sended.doc(docID).set(port_emails_sended_model,{ merge: true })
-            .then((value) => {
-                // return response to users
-                res.status(200).send({
-                    message: "email sended created successfully",
-                    data: port_emails_sended_model,
-                    error: {},
-                })
-            });
+        try {
+
+            await collection_emails_sended
+                .doc(docID)
+                .set(port_emails_sended_model,{ merge: true })
+                .then((value) => {
+                    // return response to users
+                    // res.status(200).send({
+                    //     message: "email sended created successfully",
+                    //     data: port_emails_sended_model,
+                    //     error: {},
+                    // })
+                    res.staus=200
+                    res.staus_message="email sended created successfully"
+                    return res;
+                });
+
+        }catch (e) {
+            res.error = e.toString()
+            return res;
+        }
 
         // return res;
 
