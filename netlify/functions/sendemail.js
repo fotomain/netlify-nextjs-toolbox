bb7c7cfd4888e7e3a8a3b3ebf002d669693b5105
fotomain <foto888999@gmail.com>
2023-11-07 13:04:23 +0200

    ff

--- netlify/functions/sendemail.js
+++ netlify/functions/sendemail.js
@@ -1,7 +1,7 @@
 
 
 var admin = require("firebase-admin");
-var serviceAccount = require("admin_key.json");
+var serviceAccount = require("./admin_key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
