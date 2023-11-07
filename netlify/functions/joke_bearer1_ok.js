8b544f6c2d04115d5741b099e5881582335fda6b
fotomain <foto888999@gmail.com>
2023-11-07 10:19:56 +0200

    ff

--- netlify/functions/joke_bearer1_ok.js
+++ netlify/functions/joke_bearer1_ok.js
@@ -4,21 +4,22 @@ export default async (req, ctx) => {
     if (req.method === "POST")
     {
 
-        var oo_='---'
-        const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
-        if (allowedOrigins.includes(req.origin)) {
-            // res.setHeader('Access-Control-Allow-Origin', origin);
-            oo_=req.origin
-        }
+        // var oo_='---'
+        // const allowedOrigins = ['http://localhost:3000','http://localhost:3001','https://port555.netlify.app'];
+        // if (allowedOrigins.includes(req.origin)) {
+        //     // res.setHeader('Access-Control-Allow-Origin', origin);
+        //     oo_=req.origin
+        // }
         const req_data = await req.json();
         const res = ctx.json({
             statusCode: 200,
             message: '!!!!!!!!!!!!!!! you posted!',
             req_data: req_data,
-            reqorigin: oo_,
+            // reqorigin: oo_,
         });
 
 
+        res.headers.set("Access-Control-Allow-Origin", "*");
         res.headers.set("Access-Control-Allow-Origin", "https://port555.netlify.app");
         res.headers.append("Access-Control-Allow-Headers", "*");
         res.headers.append("Access-Control-Allow-Methods", "*");
